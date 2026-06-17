b404f67de85043498d5be7e00519c29379f61ce5
GabrielVelasco <themrgabriel100@gmail.com>
2026-06-16 22:17:47 -0300

    magad

--- scripts/apiConfig.js
+++ scripts/apiConfig.js
@@ -1,28 +1,32 @@
 const API_CONFIG = {
-    baseUrl: 'https://e1a9-2804-4b10-501-e600-fc5f-9557-efe5-b898.ngrok-free.app',
-};
-
-// Helper function to make API requests
-async function getLiveEventsList() {
-
-    try{
-        return await axios.get(`${API_CONFIG.baseUrl}/api/live`);
-
-    }catch (error) {
-        console.error(`Error making API request:`, error);
-        throw error;
+    baseUrl: "https://e1a9-2804-4b10-501-e600-fc5f-9557-efe5-b898.ngrok-free.app",
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+      "Accept": "application/json"
     }
-}
+  };
 
-async function getSatsFromAPI(matchID) {
-
-    try{
-        return await axios.get(`${API_CONFIG.baseUrl}/api/${matchID}/stats`);
-
-    }catch (error) {
-        console.error(`Error making API request:`, error);
-        throw error;
+  // Helper function to make API requests
+  async function getLiveEventsList() {
+    try {
+      return await axios.get(`${API_CONFIG.baseUrl}/api/live`, {
+        headers: API_CONFIG.headers
+      });
+    } catch (error) {
+      console.error("Error making API request:", error);
+      throw error;
     }
-}
+  }
 
-export { getLiveEventsList, getSatsFromAPI };
+  async function getSatsFromAPI(matchID) {
+    try {
+      return await axios.get(`${API_CONFIG.baseUrl}/api/${matchID}/stats`, {
+        headers: API_CONFIG.headers
+      });
+    } catch (error) {
+      console.error("Error making API request:", error);
+      throw error;
+    }
+  }
+
+  export { getLiveEventsList, getSatsFromAPI };
