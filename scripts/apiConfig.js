918a8e92b26fa97754042dc30dce78b71974aacb
GabrielVelasco <themrgabriel100@gmail.com>
2026-07-06 18:52:53 -0300

    Update API URL to https://1241-2804-4b10-500-7100-83f4-6a64-8af1-74ba.ngrok-free.app

--- scripts/apiConfig.js
+++ scripts/apiConfig.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "https://8226-2804-4b10-503-aa00-a370-daa5-3663-b3e4.ngrok-free.app";
+const DEFAULT_API_BASE_URL = "https://1241-2804-4b10-500-7100-83f4-6a64-8af1-74ba.ngrok-free.app";
 
 const API_HEADERS = {
     "ngrok-skip-browser-warning": "true",
